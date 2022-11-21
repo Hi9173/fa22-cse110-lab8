643c5761511bdc7b91f252398bd50992435d9eff
Justin <zeh003@ucsd.edu>
2022-11-20 21:50:58 -0800

    Update expose and explore

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Lab 8 - Starter
+Question 1:
+Within a Github action that runs whenever code is pushed. By doing so, Github will run an automatic test every time a piece of code is pushed to a repo and making sure no crashes is happening. Also if something wrong happens, it is very easy to backtrack the point of where the problem occured.
+
+Question 2:
+No, because an end to end test is meant to test the interaction bewteen the users and the software, ie the flow of how the user would use the software from begin to end. This does not require certain functions to return the correct value.
+
+Question 3:
+No, we should not use Unit Test to test the "message" feature. This is because the "message" feature requires interaction between users, which is not what Unit Test are meant to test.
+
+Question 4:
+Yes, Unit Test will be appropriate for testing the "max length message" feature. This is because all we are doing in this feature is limiting the max length, which can be easily done by returning True/False in a Unit Test function.
+
+![image Info](file://../screenshots/la8.test.png "Screenshot of lab8 test")
+![image Info](file://../screenshots/sum.test.png "Screenshot of sum test")
+![image Info](file://../screenshots/la8.unit.png "Screenshot of unit test")
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,67 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+test('(702)885-8022 is a phone number', () => {
+    expect(functions.isPhoneNumber('(702)885-8022')).toBe(true);
+});
+test('(949)998-0857 is a phone number', () => {
+    expect(functions.isPhoneNumber('(949)998-0857')).toBe(true);
+});
+test('9238-193-ab is not a phone number', () => {
+    expect(functions.isPhoneNumber('9238-193-ab')).toBe(false);
+});
+test('(mox1@30Am0 is not a phone number', () => {
+    expect(functions.isPhoneNumber('mox1@30Am0')).toBe(false);
+});
+
+test('zeh003@ucsd.edu is an email address', () => {
+    expect(functions.isEmail('zeh003@ucsd.edu')).toBe(true);
+});
+test('zehmessage@gmail.com is an email address', () => {
+    expect(functions.isEmail('zehmessage@gmail.com')).toBe(true);
+});
+test('gmai@oasdcom is not an email address', () => {
+    expect(functions.isEmail('gmai@oasdcom')).toBe(false);
+});
+test('298a@9g0.com2#o is not an email address', () => {
+    expect(functions.isEmail('298a@9g0.com2#o')).toBe(false);
+});
+
+test('A16375645 is a strong password', () => {
+    expect(functions.isStrongPassword('A16375645')).toBe(true);
+});
+test('Strong_PASS0811 is a strong password', () => {
+    expect(functions.isStrongPassword('Strong_PASS0811')).toBe(true);
+});
+test('8ASD028 is not a strong password', () => {
+    expect(functions.isStrongPassword('8ASD028')).toBe(false);
+});
+test('ISOFSOIEFOIEJFSOEOFJ91492 is not a strong password', () => {
+    expect(functions.isStrongPassword('ISOFSOIEFOIEJFSOEOFJ91492')).toBe(false);
+});
+
+test('11/20/2022 is a date', () => {
+    expect(functions.isDate('11/20/2022')).toBe(true);
+});
+test('1/1/2000 is a date', () => {
+    expect(functions.isDate('1/1/2000')).toBe(true);
+});
+test('922/10/2014 is not a date', () => {
+    expect(functions.isDate('922/10/2001')).toBe(false);
+});
+test('A1/23/2009 is not a date', () => {
+    expect(functions.isDate('A1/23/2009')).toBe(false);
+});
+
+test('#FF0000 is a color', () => {
+    expect(functions.isHexColor('#FF0000')).toBe(true);
+});
+test('#800080 is a color', () => {
+    expect(functions.isHexColor('#800080')).toBe(true);
+});
+test('#FFF000F0 is not a color', () => {
+    expect(functions.isHexColor('#FFF000F0')).toBe(false);
+});
+test('a is not a color', () => {
+    expect(functions.isHexColor('a')).toBe(false);
+});
